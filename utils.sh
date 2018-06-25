d378d77eac51644c5595f066c8892818bff2db0b
Joe Garcia, CISSP <infamousjoeg@users.noreply.github.com>
2018-06-25 13:11:28 -0400

    Create utils.sh

---
+++ utils.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+announce() {
+  echo "++++++++++++++++++++++++++++++++++++++"
+  echo ""
+  echo "$@"
+  echo ""
+  echo "++++++++++++++++++++++++++++++++++++++"
+}
+
+run_conjur_cmd_as_admin() {
+  local command=$(cat $@)
+
+  conjur authn logout > /dev/null
+  conjur authn login -u admin -p "$CONJUR_ADMIN_PASSWORD" > /dev/null
+
+  local output=$(eval "$command")
+
+  conjur authn logout > /dev/null
+  echo "$output"
+}
+
+load_policy() {
+  local POLICY_FILE=$1
+
+  run_conjur_cmd_as_admin <<CMD
+conjur policy load --as-group security_admin "policy/$POLICY_FILE"
+CMD
+}
+
+rotate_host_api_key() {
+  local host=$1
+
+  run_conjur_cmd_as_admin <<CMD
+conjur host rotate_api_key -h $host
+CMD
+}
