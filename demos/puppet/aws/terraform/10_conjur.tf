39501465666f28c21d868111b5c173859979f7e6
Micah Lee <micah.lee@cyberark.com>
2019-06-05 15:06:50 -0400

    !fixup No longer specificy AMI argument to conjur master module

--- demos/puppet/aws/terraform/10_conjur.tf
+++ demos/puppet/aws/terraform/10_conjur.tf
@@ -3,7 +3,6 @@ module "conjur_master" {
 
   vpc_id = "${var.vpc_id}"
   resource_prefix = "${var.resource_prefix}"
-  conjur_ee_ami_id = "${var.conjur_ee_ami_id}"
   ssh_key_name = "${aws_key_pair.generated_key.key_name}"
   ssh_key_pem = "${tls_private_key.ssh_access_key.private_key_pem}"
 }
