dd9fa79a0c6c5bd552cc96c8cfa7a11f612dc983
Kate MacInnis <macinnis.k@gmail.com>
2022-12-13 13:10:27 -0700

    Show only needed columns

--- repsetdata.sh
+++ repsetdata.sh
@@ -5,9 +5,7 @@ SELECT_STATEMENT=$(cat <<EOF
         SELECT  
           RTRIM(LTRIM(REPLACE(CAST(replication_sets AS varchar), '{}', 'Full' ), '{'),'}') AS "ReplicationSets", 
           policy_id AS "Policy",
-          account(resource_id) as "Account",
-          kind(resource_id) as "Kind",
-          identifier(resource_id) as "Variable"
+          identifier(resource_id) as "Variable",
         FROM resources 
           WHERE 
             policy_id NOT LIKE 'system%'
