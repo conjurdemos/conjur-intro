6e6cf35bd722e0b0d20ff84befbb3954e11fe216
Kate MacInnis <macinnis.k@gmail.com>
2022-12-13 17:30:53 -0700

    Remove trailing spaces

--- repsetdata.sh
+++ repsetdata.sh
@@ -1,20 +1,22 @@
 #!/bin/bash
 
-OUTPUT_NAME="variables-by-replication-set-$(date +"%Y_%m_%d_%I_%M_%s").csv" 
+set -euo pipefail
+
+OUTPUT_NAME="variables-by-replication-set-$(date +"%Y_%m_%d_%I_%M_%s").csv"
 SELECT_STATEMENT=$(cat <<EOF
-        SELECT  
-          RTRIM(LTRIM(REPLACE(CAST(replication_sets AS varchar), '{}', 'Full' ), '{'),'}') AS "ReplicationSets", 
+        SELECT
+          RTRIM(LTRIM(REPLACE(CAST(replication_sets AS varchar), '{}', 'Full' ), '{'),'}') AS "ReplicationSets",
           policy_id AS "Policy",
-          identifier(resource_id) as "Variable",
-        FROM resources 
-          WHERE 
+          identifier(resource_id) as "Variable"
+        FROM resources
+          WHERE
             policy_id NOT LIKE 'system%'
-          AND 
+          AND
             resource_id LIKE '%:variable:%'
         ORDER BY replication_sets, resource_id ASC
 EOF
 )
-     
+
 parse_parameters() {
   POSITIONAL_ARGS=()
 
@@ -28,8 +30,8 @@ parse_parameters() {
       shift ;;
     -h | --help ) print_help
       shift ;;
-    --) shift; 
-        break 
+    --) shift;
+        break
         ;;
     -*|--*)
       echo "Unknown option $1"
@@ -38,11 +40,11 @@ parse_parameters() {
     *)
       POSITIONAL_ARGS+=("$1") # save positional arg
       shift # past argument
-      ;;    
+      ;;
     esac
   done
   if [ ${#POSITIONAL_ARGS[@]} -eq 1 ]; then
-      CONTAINER_NAME=${POSITIONAL_ARGS[0]}
+    CONTAINER_NAME=${POSITIONAL_ARGS[0]}
   else
     echo "There must be exacly one positional argument. Run $0 for more information."
     exit 1
@@ -53,7 +55,7 @@ print_help() {
   cat <<EOF
 Generates a csv of replication sets and the variables that belong to them.
 Usage: repsetdata.sh <appliance-container> [options]
-    --podman          Indicates container is Podman
+    --podman          Indicates container runtime is Podman
     -h, --help        Shows this help message.
 EOF
   exit
@@ -64,9 +66,8 @@ main() {
   parse_parameters "$@"
   CONTAINERS_PLATFORM="${CONTAINERS_PLATFORM:-docker}"
   CONTAINER_NAME="${CONTAINER_NAME:-ERROR}"
-  "$CONTAINERS_PLATFORM" exec --user conjur "$CONTAINER_NAME" psql -c "COPY ($SELECT_STATEMENT) TO STDOUT CSV HEADER" > $OUTPUT_NAME && {
-    echo "Saved to $OUTPUT_NAME"
-  }
+  "$CONTAINERS_PLATFORM" exec --user conjur "$CONTAINER_NAME" psql -c "COPY ($SELECT_STATEMENT) TO STDOUT CSV HEADER" > $OUTPUT_NAME
+  echo "Saved to $OUTPUT_NAME"
 }
 
 
