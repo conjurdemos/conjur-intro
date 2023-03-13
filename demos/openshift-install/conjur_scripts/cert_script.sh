91c863e86ff4745fd1a495dc9bb88b78b8768844
Shlomo Heigh <shlomo.heigh@cyberark.com>
2023-03-13 14:57:49 -0400

    Use Conjur CLI v8.0

--- demos/openshift-install/conjur_scripts/cert_script.sh
+++ demos/openshift-install/conjur_scripts/cert_script.sh
@@ -26,6 +26,6 @@ openssl req -x509 -new -nodes -key ca.key -sha1 -days 3650 -set_serial 0x0 -out 
 openssl x509 -in ca.cert -text -noout &> /dev/null
 
 # Load variable values
-conjur variable values add conjur/authn-k8s/$AUTHENTICATOR_ID/ca/key "$(cat ca.key)"
-conjur variable values add conjur/authn-k8s/$AUTHENTICATOR_ID/ca/cert "$(cat ca.cert)"
+conjur variable set -i conjur/authn-k8s/$AUTHENTICATOR_ID/ca/key -v "$(cat ca.key)"
+conjur variable set -i conjur/authn-k8s/$AUTHENTICATOR_ID/ca/cert -v "$(cat ca.cert)"
 
