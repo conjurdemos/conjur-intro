0c805d3ed5808a78fa06a4534d8f465686bb9a44
Hubert Dąbrowski <hubert.dabrowski@cyberark.com>
2024-01-24 14:53:14 +0100

    Migrate from jmeter to k6

---
+++ files/k6/load-policy.js
@@ -0,0 +1,62 @@
+import http from "k6/http";
+import { check } from "k6";
+import * as conjurApi from "./modules/api.js";
+import * as lib from "./modules/lib.js";
+
+/**
+ *  Init stage
+ */
+const requiredEnvVars = [
+  "POLICY_FILE",
+  "POLICY_ID"
+];
+
+lib.checkRequiredEnvironmetVariables(requiredEnvVars)
+
+const policyFile = lib.get_env_var("POLICY_FILE")
+const policyId = lib.get_env_var("POLICY_ID")
+const policyContent = open(policyFile);
+
+export const options = {
+  scenarios: {
+    policy: {
+      executor: 'per-vu-iterations',
+      vus: 1,
+      iterations: 1,
+      // 6 hours
+      maxDuration: '21600s',
+    },
+  },
+};
+
+export default function () {
+  const settings = lib.parse_env();
+  let authRes;
+
+  // authn to obtain token
+  authRes = conjurApi.authenticate(
+    http,
+    settings,
+    true
+  );
+
+  check(authRes, {
+    "status is 200": (r) => r.status === 200,
+  });
+
+  settings.token = authRes.body
+
+  // create policy
+  const lobsPolicyRes = conjurApi.load_policy(
+    http,
+    settings,
+    policyId,
+    policyContent
+  );
+
+  console.log("RESPONSE BODY START", lobsPolicyRes.body, "RESPONSE BODY END")
+
+  check(lobsPolicyRes, {
+    "status is 201": (r) => r.status === 201,
+  });
+}
---
+++ files/k6/modules/api.js
@@ -0,0 +1,120 @@
+import { check, fail } from "k6";
+
+// Returns an http response. This allows assertions on the response to be made.
+export function authenticate(client, data, exitOnFailure=false) {
+  const {
+    applianceUrl,
+    conjurAccount,
+    conjurIdentity,
+    apiKey,
+    authenticateTrend,
+    authenticateFailRate
+  } = data;
+
+  const headers = { 'Accept-Encoding': 'base64' }
+  const res = client.post(
+    `${applianceUrl}/authn/${conjurAccount}/${conjurIdentity}/authenticate`,
+    apiKey,
+    {
+      headers,
+      tags: { endpoint: 'PostAuthnURL' },
+      // timeout: "500s" // Changes k6 status 0 error_code 1050 to HTTP 504
+    }
+  )
+
+  // If given a trend or rate, be sure to add it here
+  authenticateTrend ? authenticateTrend.add(res.timings.duration) : null;
+  authenticateFailRate ? authenticateFailRate.add(res.status !== 200) : null;
+
+  // Fail if the authn request did not return a token
+  if(exitOnFailure){
+    if ( !check(res, {
+      "status is 200": (r) => r.status === 200,
+    })){
+      fail(`Authn request failed with status '${res.status}' and status_text: '${res.status_text}'. Stopping this iteration.`)
+    }
+  }
+
+  return res
+}
+
+export function load_policy(client, data, policy_id, policy_body) {
+  const {
+    applianceUrl,
+    conjurAccount,
+    token
+  } = data;
+  const headers = { 'Authorization': `Token token="${token}"` }
+  const res = client.post(
+    `${applianceUrl}/policies/${conjurAccount}/policy/${policy_id}`,
+    policy_body,
+    {
+      headers,
+      timeout: '1h',
+      tags: { endpoint: 'PostPoliciesURL' },
+    }
+  )
+  return res
+}
+
+export function write_secret(client, data, identity, body) {
+  const {
+    applianceUrl,
+    conjurAccount,
+    token
+  } = data;
+
+  const headers = { 'Authorization': `Token token="${token}"` }
+
+  const res = client.post(
+    `${applianceUrl}/secrets/${conjurAccount}/variable/${encodeURIComponent(identity)}`,
+    body,
+    {
+      headers,
+      tags: { endpoint: 'PostSecretsURL' },
+    }
+  )
+
+  return res
+}
+
+export function read_secret(client, data, identity) {
+  const {
+    applianceUrl,
+    conjurAccount,
+    token
+  } = data;
+
+  const headers = { 'Authorization': `Token token="${token}"` };
+  const url = `${applianceUrl}/secrets/${conjurAccount}/variable/${encodeURIComponent(identity)}`;
+
+  const res = client.get(
+    url,
+    {
+      headers,
+      tags: { endpoint: 'GetSecretsURL' }
+    }
+  )
+
+  return res
+}
+
+export function get(client, data, path) {
+  const {
+    applianceUrl,
+    token
+  } = data;
+
+  const headers = { 'Authorization': `Token token="${token}"` };
+  const url = `${applianceUrl}/${path}`;
+
+  const res = client.get(
+      url,
+      {
+        headers,
+        tags: { endpoint: 'GetSecretsURL' }
+      }
+  )
+
+  return res
+}
